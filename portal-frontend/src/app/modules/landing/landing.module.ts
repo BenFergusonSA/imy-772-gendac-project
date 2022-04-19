21e56d982dba130397f27e853fde369fcb4adb74
Benedict Ferguson <benedict.ferguson@gmail.com>
2022-04-20 00:30:37 +0200

    added shared module

--- portal-frontend/src/app/modules/landing/landing.module.ts
+++ portal-frontend/src/app/modules/landing/landing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { LandingRoutingModule } from './landing-routing.module';
 import {LandingPageComponent} from "./pages/landing-page/landing-page.component";
+import {SharedModule} from "../../shared/shared.module";
 
 @NgModule({
-  imports: [LandingRoutingModule],
+  imports: [LandingRoutingModule, SharedModule],
   declarations: [LandingPageComponent],
 })
 export class LandingModule { }
