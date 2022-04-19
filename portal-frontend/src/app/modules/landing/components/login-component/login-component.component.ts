27261a12cf90fa23c9346da9bd7fd1d1ab9875ac
Benedict Ferguson <benedict.ferguson@gmail.com>
2022-04-20 01:31:07 +0200

    added basic styling and form

--- portal-frontend/src/app/modules/landing/components/login-component/login-component.component.ts
+++ portal-frontend/src/app/modules/landing/components/login-component/login-component.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-login-component',
@@ -6,10 +8,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login-component.component.less']
 })
 export class LoginComponentComponent implements OnInit {
+  validateForm!: FormGroup;
 
-  constructor() { }
+  constructor(private fb: FormBuilder, private router: Router) {}
 
   ngOnInit(): void {
+    this.validateForm = this.fb.group({
+      userName: [null, [Validators.required]],
+      password: [null, [Validators.required]],
+      remember: [true]
+    });
   }
 
+  submitForm(): void {
+    if (this.validateForm.valid) {
+      this.router.navigate(['portal', 'dashboard']);
+    } else {
+      Object.values(this.validateForm.controls).forEach(control => {
+        if (control.invalid) {
+          control.markAsDirty();
+          control.updateValueAndValidity({ onlySelf: true });
+        }
+      });
+    }
+  }
 }
