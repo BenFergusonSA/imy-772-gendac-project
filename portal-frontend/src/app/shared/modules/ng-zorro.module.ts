763a2f991902dc1d1e604c784fc8aaefea38649c
Benedict Ferguson <benedict.ferguson@gmail.com>
2022-04-20 01:30:42 +0200

    added ng zorro module imports

--- portal-frontend/src/app/shared/modules/ng-zorro.module.ts
+++ portal-frontend/src/app/shared/modules/ng-zorro.module.ts
@@ -2,11 +2,17 @@ import { NgModule } from '@angular/core';
 
 import { NzLayoutModule } from 'ng-zorro-antd/layout';
 import { NzMenuModule } from 'ng-zorro-antd/menu';
+import {NzFormModule} from "ng-zorro-antd/form";
+import {NzButtonModule} from "ng-zorro-antd/button";
+import {NzInputModule} from "ng-zorro-antd/input";
 
 @NgModule({
   exports: [
     NzLayoutModule,
-    NzMenuModule
+    NzMenuModule,
+    NzFormModule,
+    NzButtonModule,
+    NzInputModule,
   ]
 })
 export class NgZorroModule { }
