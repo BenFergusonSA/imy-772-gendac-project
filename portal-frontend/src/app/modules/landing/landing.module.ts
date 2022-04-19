3edb30284e110b0ce215eb6357e2ec75ced10ee4
Benedict Ferguson <benedict.ferguson@gmail.com>
2022-04-20 00:36:31 +0200

    added login component

--- portal-frontend/src/app/modules/landing/landing.module.ts
+++ portal-frontend/src/app/modules/landing/landing.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { LandingRoutingModule } from './landing-routing.module';
 import {LandingPageComponent} from "./pages/landing-page/landing-page.component";
 import {SharedModule} from "../../shared/shared.module";
+import { LoginComponentComponent } from './components/login-component/login-component.component';
 
 @NgModule({
   imports: [LandingRoutingModule, SharedModule],
-  declarations: [LandingPageComponent],
+  declarations: [LandingPageComponent, LoginComponentComponent],
 })
 export class LandingModule { }
