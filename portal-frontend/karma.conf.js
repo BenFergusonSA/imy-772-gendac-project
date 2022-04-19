72cd059b792bfaeb016db0d266aa46ab9a1cd213
Benedict Ferguson <benedict.ferguson@gmail.com>
2022-04-19 23:06:55 +0200

    converted angular frontend to less

--- portal-frontend/karma.conf.js
+++ portal-frontend/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/gendac'),
+      dir: require('path').join(__dirname, './coverage/portal-frontend'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- portal-frontend/src/app/app.component.spec.ts
+++ portal-frontend/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'gendac'`, () => {
+  it(`should have as title 'portal-frontend'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('gendac');
+    expect(app.title).toEqual('portal-frontend');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('gendac app is running!');
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('.content span')?.textContent).toContain('portal-frontend app is running!');
   });
 });
