b0c61fde9342f531c846834d1ea4a75b5eea0643
Benedict Ferguson <benedict.ferguson@gmail.com>
2022-05-01 12:43:49 +0200

    removed hub listener

--- portal-frontend/src/app/shared/services/auth.service.ts
+++ portal-frontend/src/app/shared/services/auth.service.ts
@@ -1,44 +1,21 @@
-import { Injectable } from '@angular/core';
-import { Auth, Hub } from 'aws-amplify';
+import {Injectable} from '@angular/core';
+import {Auth, Hub} from 'aws-amplify';
+import {BehaviorSubject} from "rxjs";
+import {CognitoUser} from "@aws-amplify/auth";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  currentAuthenticatedUser: BehaviorSubject<CognitoUser | null> = new BehaviorSubject<CognitoUser | null>(null);
 
   constructor() {
-    Hub.listen('auth', this.handleHubEvent);
-  }
-
-  handleHubEvent(data: any) {
-    switch (data.payload.event) {
-      case 'signIn':
-        console.info('user signed in');
-        break;
-      case 'signUp':
-        console.info('user signed up');
-        break;
-      case 'signOut':
-        console.info('user signed out');
-        break;
-      case 'signIn_failure':
-        console.error('user sign in failed');
-        break;
-      case 'tokenRefresh':
-        console.info('token refresh succeeded');
-        break;
-      case 'tokenRefresh_failure':
-        console.error('token refresh failed');
-        break;
-      case 'configured':
-        console.info('the Auth module is configured');
-    }
   }
 
   async signIn(username: string, password: string): Promise<boolean> {
     try {
-      const user = await Auth.signIn(username, password);
-      console.log('USER', user);
+      const user: CognitoUser = await Auth.signIn(username, password);
+      this.currentAuthenticatedUser.next(user);
       return true;
     } catch (error) {
       console.log('error signing in', error);
