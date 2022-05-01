2f870ad36b95c9714d028e89b12c799b3d00bc29
Benedict Ferguson <benedict.ferguson@gmail.com>
2022-05-01 12:02:44 +0200

    added amplify auth

--- portal-frontend/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ portal-frontend/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,13 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "gendacportal58050eae": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
