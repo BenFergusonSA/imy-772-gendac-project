df05f3de136c3bcba60a2eab849970e0bb552a00
Benedict Ferguson <benedict.ferguson@gmail.com>
2022-05-01 12:03:48 +0200

    updated to use auth service for login

--- portal-frontend/src/app/modules/landing/components/login-component/login-component.component.ts
+++ portal-frontend/src/app/modules/landing/components/login-component/login-component.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 import {Router} from "@angular/router";
+import {AuthService} from "../../../../shared/services/auth.service";
 
 @Component({
   selector: 'app-login-component',
@@ -10,26 +11,30 @@ import {Router} from "@angular/router";
 export class LoginComponentComponent implements OnInit {
   validateForm!: FormGroup;
 
-  constructor(private fb: FormBuilder, private router: Router) {}
+  constructor(private fb: FormBuilder, private router: Router, private authService: AuthService) {}
 
   ngOnInit(): void {
     this.validateForm = this.fb.group({
-      userName: [null, [Validators.required]],
+      email: [null, [Validators.required]],
       password: [null, [Validators.required]],
       remember: [true]
     });
   }
 
-  submitForm(): void {
-    if (this.validateForm.valid) {
-      this.router.navigate(['portal', 'dashboard']);
-    } else {
-      Object.values(this.validateForm.controls).forEach(control => {
-        if (control.invalid) {
-          control.markAsDirty();
-          control.updateValueAndValidity({ onlySelf: true });
-        }
-      });
+  async submitForm(): Promise<void> {
+    if (!this.validateForm.valid) {
+      return;
     }
+
+    const {email, password} = this.validateForm.getRawValue();
+
+    const hasSignedInSuccessfully = await this.authService.signIn(email, password);
+
+    if (!hasSignedInSuccessfully) {
+      console.error('FAILED TO LOGIN');
+      return;
+    }
+
+    this.router.navigate(['portal', 'dashboard']);
   }
 }
