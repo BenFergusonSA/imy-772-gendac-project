f0a99cd27b37f618137b6edd179ca0cf341bf77e
Benedict Ferguson <benedict.ferguson@gmail.com>
2022-05-17 07:12:35 +0200

    Added zorro modules for upload

--- portal-frontend/src/app/shared/modules/ng-zorro.module.ts
+++ portal-frontend/src/app/shared/modules/ng-zorro.module.ts
@@ -6,6 +6,10 @@ import {NzFormModule} from "ng-zorro-antd/form";
 import {NzButtonModule} from "ng-zorro-antd/button";
 import {NzInputModule} from "ng-zorro-antd/input";
 import { NzAlertModule } from 'ng-zorro-antd/alert';
+import { NzPopoverModule } from 'ng-zorro-antd/popover';
+import { NzSpaceModule } from 'ng-zorro-antd/space';
+import { NzUploadModule } from 'ng-zorro-antd/upload';
+import { NzNotificationModule } from 'ng-zorro-antd/notification';
 
 @NgModule({
   exports: [
@@ -14,7 +18,11 @@ import { NzAlertModule } from 'ng-zorro-antd/alert';
     NzFormModule,
     NzButtonModule,
     NzInputModule,
-    NzAlertModule
+    NzAlertModule,
+    NzPopoverModule,
+    NzSpaceModule,
+    NzUploadModule,
+    NzNotificationModule
   ]
 })
 export class NgZorroModule { }
