b794ca4903c2e830dc99ad149476f3387552fb14
Benedict Ferguson <benedict.ferguson@gmail.com>
2022-05-17 07:11:11 +0200

    added logout functionality

--- portal-frontend/src/app/shared/services/auth.service.ts
+++ portal-frontend/src/app/shared/services/auth.service.ts
@@ -15,15 +15,36 @@ export class AuthService {
   async signIn(username: string, password: string): Promise<boolean> {
     try {
       const user: CognitoUser = await Auth.signIn(username, password);
+      console.log('User', user);
       this.currentAuthenticatedUser.next(user);
       return true;
     } catch (error) {
-      console.log('error signing in', error);
+      console.error('Error signing in', error);
       return false;
+    }
+  }
+
+  async signOut(): Promise<void> {
+    try {
+      await Auth.signOut();
+      this.currentAuthenticatedUser.next(null);
+    } catch (error) {
+      console.error('Error signing in', error);
     }
   }
 
   isAuthenticated() {
     return this.currentAuthenticatedUser.getValue() !== null;
+  }
+
+  async getUser() {
+    try {
+      const user: CognitoUser = await Auth.currentAuthenticatedUser({
+        bypassCache: false  // Optional, By default is false. If set to true, this call will send a request to Cognito to get the latest user data
+      })
+      this.currentAuthenticatedUser.next(user);
+    } catch (error) {
+      console.error('Error getting user', error);
+    }
   }
 }
