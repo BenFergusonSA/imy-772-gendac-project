59198927fd915f3aac73be3ef20bc4fb2bcf0dee
Vashist Lalla <vashistlalla@outlook.com>
2022-05-31 15:57:22 +0200

    filename now uses uuid generation

--- portal-frontend/src/app/modules/upload-cv/services/upload-cv.service.ts
+++ portal-frontend/src/app/modules/upload-cv/services/upload-cv.service.ts
@@ -1,6 +1,7 @@
 import {Injectable} from '@angular/core';
 import {Storage} from 'aws-amplify';
 import {NzUploadFile} from "ng-zorro-antd/upload";
+import * as uuid from "uuid";
 
 @Injectable({
   providedIn: 'root'
@@ -12,7 +13,7 @@ export class UploadCvService {
 
   async uploadSingleCv(file: NzUploadFile) {
     const timeStamp = new Date().toISOString();
-    const fileName = `${timeStamp}-${file.name}`;
+    const fileName = uuid.v4();
     try {
       await Storage.put(fileName, file);
     } catch (error) {
