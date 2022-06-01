9337a0ba094acbb7f5953b4a7346506b9214d914
Deannortje <u17326622@tuks.co.za>
2022-06-01 09:03:50 +0200

    Revert "Merge remote-tracking branch 'origin/feature/add-components' into feature/add-components"
    This reverts commit e08032cf4dfa2c201557e3a3f1ee4778cbaacf0a, reversing
    changes made to e08b04af2bd1ff32360a2b5ddb49d0b803f690f1.
    
    # Conflicts:
    #	portal-frontend/src/app/modules/upload-cv/services/upload-cv.service.ts

--- portal-frontend/src/app/modules/upload-cv/services/upload-cv.service.ts
+++ portal-frontend/src/app/modules/upload-cv/services/upload-cv.service.ts
@@ -1,6 +1,6 @@
-import { Injectable } from '@angular/core';
-import {HttpClient} from "@angular/common/http";
-import {API_ENDPOINTS} from "../../../shared/constants/api-endpoints.constant";
+import {Injectable} from '@angular/core';
+import {Storage} from 'aws-amplify';
+import {NzUploadFile} from "ng-zorro-antd/upload";
 
 @Injectable({
   providedIn: 'root'
@@ -9,10 +9,14 @@ export class UploadCvService {
 
   constructor(private httpClient: HttpClient) { }
 
-  uploadSingleCv(formData: FormData) {
-    return this.httpClient.post(API_ENDPOINTS.uploadCv, formData, {
-      reportProgress: true,
-      observe: 'events'
-    })
+  async uploadSingleCv(file: NzUploadFile) {
+    const timeStamp = new Date().toISOString();
+    const fileName = `${timeStamp}-${file.name}`;
+    try {
+      await Storage.put(fileName, file);
+    } catch (error) {
+      console.log("Error uploading file: ", error);
+      throw Error('Failed to upload CV');
+    }
   }
 }
