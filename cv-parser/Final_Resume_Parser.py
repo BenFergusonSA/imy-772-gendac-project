7f345b669fc2152072ef9d95a3b6b3305a27fa65
Vashist Lalla <vashistlalla@outlook.com>
2022-06-22 19:30:54 +0200

    Add education extractor to cv-parser
    searches through document to find education that is specified in the array

--- cv-parser/Final_Resume_Parser.py
+++ cv-parser/Final_Resume_Parser.py
@@ -2,9 +2,16 @@ from pdfminer.high_level import extract_text
 import spacy
 from spacy import displacy
 import json
+import re
 
 def convertToText(fname):
     return extract_text(fname)
+
+EDUCATION = [
+    'B.Eng', 'BEng' ,'B.Eng Electrical', 'B.Eng Electronic', 'B.Eng Computer', 'BEng Electrical', 'BEng Electronic', 'BEng Computer',
+    'BSc Computer Science', 'B.Sc Computer Science', 'BCom Informatics', 'B.Com Informatics', 'BIS',
+    'B Information Systems', 'BIT', 'B Information Technology', 'Multimedia', 'BSc', 'B.Sc', 'BSc Information and Knowledge Systems', 'B.Sc Information and Knowledge Systems'
+]
 
 nlp = spacy.load("en_core_web_lg")
 
@@ -16,7 +23,12 @@ ruler.from_disk(skills)
 
 text = convertToText("../cv-backend/storage/temp.pdf")
 doc = nlp(text)
-#displacy.serve(doc, style="ent")
+
+#filters unnecessary stop words and remove special characters
+filtered = [token.text for token in doc if token.is_stop == False]
+text_no_punt = [re.sub('[^a-zA-Z0-9]+', '', _) for _ in filtered]
+
+#extract skills from document
 skillArrWithDups = []
 for ent in doc.ents:
     if ent.label_ == 'SKILL':
@@ -24,13 +36,30 @@ for ent in doc.ents:
 
 for i in range(len(skillArrWithDups)):
     skillArrWithDups[i] = skillArrWithDups[i].upper()
+
+#remove skill duplicates
 pureSkillArr = list(dict.fromkeys(skillArrWithDups))
+
+#extract education from document
+applicantEdu = []
+for i in text_no_punt:
+    for j in EDUCATION:
+        if j.lower() in i.lower():
+            applicantEdu.append(j)
+
+#remove education duplicates
+applicantEdu = list(dict.fromkeys(applicantEdu))
+
+print("Education")
+print(applicantEdu)
+
 
 print("Skills")
 print(pureSkillArr)
 
 results = {
-    "skills": pureSkillArr
+    "skills": pureSkillArr,
+    "education": applicantEdu
 }
 resultData = json.dumps(results)
 f = open("../cv-backend/storage/results.json", "w")
