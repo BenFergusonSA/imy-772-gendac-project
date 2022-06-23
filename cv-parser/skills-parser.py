e947e2057fa69d5288c08398a634f6dcde8f5cd9
Vashist Lalla <vashistlalla@outlook.com>
2022-06-24 00:41:12 +0200

    add skill-parser
    without education parser

---
+++ cv-parser/skills-parser.py
@@ -0,0 +1,38 @@
+from pdfminer.high_level import extract_text
+import spacy
+from spacy import displacy
+import json
+import re
+
+def convertToText(fname):
+    return extract_text(fname)
+
+nlp = spacy.load("en_core_web_lg")
+
+skills = "jz_skill_patterns.jsonl"
+
+ruler = nlp.add_pipe("entity_ruler", before="ner")
+
+ruler.from_disk(skills)
+
+text = convertToText("../cv-backend/storage/temp.pdf")
+doc = nlp(text)
+
+#extract skills from document
+skillArrWithDups = [ent.text for ent in doc.ents if ent.label_ == 'SKILL']
+[skill.upper() for skill in skillArrWithDups]
+
+#remove skill duplicates
+pureSkillArr = set(skillArrWithDups)
+
+print("Skills")
+print(pureSkillArr)
+
+results = {
+    "skills": pureSkillArr
+}
+resultData = json.dumps(results)
+print(resultData)
+f = open("../cv-backend/storage/results.json", "w")
+f.write(resultData)
+f.close()
